c59170e44406c973040030739c0c3d39a3d58e79
Ayush <bhardwaj.ayush007@gmail.com>
2025-04-22 23:26:15 +0530

    fps directional movement towards aim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -13,10 +14,12 @@ public class PlayerController : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private InputManager inputManager;
+    private Transform cameraTransform;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
         inputManager = InputManager.Instance;
     }
 
@@ -32,12 +35,20 @@ public class PlayerController : MonoBehaviour
         // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector2 movement = inputManager.GetPlayerMovement();
         Vector3 move = new(movement.x, 0, movement.y);
+        // move = Vector3.Dot(move, cameraTransform.forward) * move;
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0;
         move = Vector3.ClampMagnitude(move, 1f); // Optional: prevents faster diagonal movement
+        // if (move != Vector3.zero)
+        // {
+        //     transform.position += forward = move;
+        // }
 
-        if (move != Vector3.zero)
-        {
-            transform.forward = move;
-        }
+        // move = Vector3.RotateTowards(move, cameraTransform.forward, math.PI, 1);
+        Quaternion lookDir = Quaternion.LookRotation(cameraTransform.forward, Vector3.up);
+        lookDir.eulerAngles = new(0, lookDir.x, 0);
+        transform.rotation = lookDir;
+        Vector3 rot = transform.rotation.eulerAngles;
 
         // Jump
         if (inputManager.IsPlayerJumpedThisFrame() && groundedPlayer)
